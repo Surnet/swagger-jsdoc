fff57be848f7e061ef1e9971bef1c589c43af095
Louis Cheung <dev.louisc@gmail.com>
2015-06-05 07:51:39 -0400

    Clean up comments

--- example/routes.js
+++ example/routes.js
@@ -1,54 +1,3 @@
-
-/**
- * @swagger
- * resourcePath: /apiJs
- * description: All about API
- */
-
-/**
- * @swagger
- * path: /login
- * operations:
- *   -  httpMethod: POST
- *      summary: Login with username and password
- *      notes: Returns a user based on username
- *      responseClass: User
- *      nickname: login
- *      consumes:
- *        - text/html
- *      parameters:
- *        - name: username
- *          description: Your username
- *          paramType: query
- *          required: true
- *          dataType: string
- *        - name: password
- *          description: Your password
- *          paramType: query
- *          required: true
- *          dataType: string
- */
-exports.login = function (req, res) {
-  var user = {};
-  user.username = req.param('username');
-  user.password = req.param('password');
-  res.json(user);
-}
-
-/**
- * @swagger
- * models:
- *   User:
- *     id: User
- *     properties:
- *       username:
- *         type: String
- *       password:
- *         type: String
- */
-
-// ---------------------------------------------------------------------------------------------------------------------
-
 'use strict';
 
 /**
@@ -56,22 +5,45 @@ exports.login = function (req, res) {
  *	@param {object} app - Express app
  */
 module.exports.setup = function (app) {
+
+	/**
+	 *	@swagger
+	 */
 	app.get('/', rootHandler)
+
+	/**
+	 *  @swagger
+	 *  path: /login
+	 *  operations:
+	 *    - httpMethod: POST
+	 *      summary: Login with username and password
+	 *      notes: Returns a user based on username
+	 *      responseClass: User
+	 *      nickname: login
+	 *      consumes:
+	 *        - text/html
+	 *      parameters:
+	 *        - name: username
+	 *          description: Your username
+	 *          paramType: query
+	 *          required: true
+	 *          dataType: string
+	 *        - name: password
+	 *          description: Your password
+	 *          paramType: query
+	 *          required: true
+	 *          dataType: string
+	 */
 	app.get('/login', loginHandler);
 }
 
-/**
- *	@swagger
- *
- */
 function rootHandler(req, res) {
     res.send('Hello World!');
 });
 
-/**
- *	@swagger
- *
- */
 function loginHandler(req, res) {
-	res.send('login');
+	var user = {};
+	user.username = req.param('username');
+	user.password = req.param('password');
+	res.json(user);
 }
