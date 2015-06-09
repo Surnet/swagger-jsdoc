d6fa5331c18b3f0dd5cf3ea0255ecb8d026d93c1
Daniel Müller <daniel.mueller@surnet.ch>
2015-06-09 17:49:23 +0200

    Clean swagger spec in example

--- example/routes.js
+++ example/routes.js
@@ -18,38 +18,38 @@ module.exports.setup = function(app) {
 
 
   /**
-   *	@swagger
-   *  /:
-   *    get:
-   *      description: Returns Hello World!
-   *      responses:
-   *        200:
-   *          description: hello world
+   * @swagger
+   * /:
+   *   get:
+   *     description: Returns the homepage
+   *     responses:
+   *       200:
+   *         description: hello world
    */
   app.get('/', rootHandler);
 
 
   /**
-   *  @swagger
-   *  /login:
-   *    post:
-   *      description: Login to the application
-   *      produces:
-   *        - application/json
-   *      parameters:
-   *        - name: username
-   *          description: Username to use for login.
-   *          in: formData
-   *          required: true
-   *          type: string
-   *        - name: password
-   *          description: User's password.
-   *          in: formData
-   *          required: true
-   *          type: string
-   *      responses:
-   *        200:
-   *          description: login
+   * @swagger
+   * /login:
+   *   post:
+   *     description: Login to the application
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: username
+   *         description: Username to use for login.
+   *         in: formData
+   *         required: true
+   *         type: string
+   *       - name: password
+   *         description: User's password.
+   *         in: formData
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: login
    */
   app.post('/login', loginHandler);
 };
