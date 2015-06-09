365f12b25a842b9020bc21b5a1e3aaa77d26144e
Daniel Müller <daniel.mueller@surnet.ch>
2015-06-09 14:22:56 +0200

    Fix jscs and jshint errors

--- example/app.js
+++ example/app.js
@@ -1,47 +1,48 @@
 'use strict';
 
 // Dependencies
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    routes = require('./routes'),
-    swagger = require('../');
+var express = require('express');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
+var swagger = require('../');
 
 // Initialize express
 var app = express();
 app.use(bodyParser.json()); // To support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // To support URL-encoded bodies
-    extended: true
+  extended: true,
 }));
 
-// Swagger definition (https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md#schema)
+// Swagger definition
+// https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md
 var swaggerDefinition = {
-    info: { // API informations (required)
-        title: 'Hello World', // Title (required)
-        version: '1.0.0', // Version (required)
-        description: 'A sample API' // Description (optional)
-    },
-    host: "localhost:3000", // Host (optional)
-    basePath: "/" // Base path (optional)
-}
+  info: { // API informations (required)
+    title: 'Hello World', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'A sample API', // Description (optional)
+  },
+  host: 'localhost:3000', // Host (optional)
+  basePath: '/', // Base path (optional)
+};
 
 // Options for the swagger docs
 var options = {
-    apiDocs: '/api-docs.json', // Path to the apiDocs JSON-File (default: '/api-docs', optional)
-    swaggerUi: '/docs', // Path to the swaggerUI (default: '/docs', optional)
-    swaggerDefinition: swaggerDefinition, // Import swaggerDefinitions
-    apis: ['./routes.js'] // Path to the API docs
+  apiDocs: '/api-docs.json', // Default: '/api-docs', optional
+  swaggerUi: '/docs', // Path to the swaggerUI (default: '/docs', optional)
+  swaggerDefinition: swaggerDefinition, // Import swaggerDefinitions
+  apis: ['./routes.js'], // Path to the API docs
 };
 
-// Initialize jsdoc-express-with-swagger
+// Initialize swagger-jsdoc
 swagger.init(app, options);
 
 // Set up the routes
 routes.setup(app);
 
 // Start the server
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
--- example/routes.js
+++ example/routes.js
@@ -1,54 +1,12 @@
 'use strict';
 
 /**
- *	Sets up the routes.
- *	@param {object} app - Express app
- */
-module.exports.setup = function (app) {
-
-    /**
-     *	@swagger
-     *  /:
-     *    get:
-     *      description: Returns Hello World!
-     *      responses:
-     *        200:
-     *          description: hello world
-     */
-    app.get('/', rootHandler);
-
-    /**
-     *  @swagger
-     *  /login:
-     *    post:
-     *      description: Login to the application
-     *      produces:
-     *        - application/json
-     *      parameters:
-     *        - name: username
-     *          description: Username to use for login.
-     *          in: formData
-     *          required: true
-     *          type: string
-     *        - name: password
-     *          description: User's password.
-     *          in: formData
-     *          required: true
-     *          type: string
-     *      responses:
-     *        200:
-     *          description: login
-     */
-    app.post('/login', loginHandler);
-};
-
-/**
  *	Handler for the Homepage
  *	@param {object} req - Express request
  *  @param {object} res - Express response
  */
 function rootHandler(req, res) {
-    res.send('Hello World!');
+  res.send('Hello World!');
 }
 
 /**
@@ -57,5 +15,47 @@ function rootHandler(req, res) {
  *  @param {object} res - Express response
  */
 function loginHandler(req, res) {
-    res.json(req.body);
+  res.json(req.body);
 }
+
+/**
+ *	Sets up the routes.
+ *	@param {object} app - Express app
+ */
+module.exports.setup = function(app) {
+
+  /**
+   *	@swagger
+   *  /:
+   *    get:
+   *      description: Returns Hello World!
+   *      responses:
+   *        200:
+   *          description: hello world
+   */
+  app.get('/', rootHandler);
+
+  /**
+   *  @swagger
+   *  /login:
+   *    post:
+   *      description: Login to the application
+   *      produces:
+   *        - application/json
+   *      parameters:
+   *        - name: username
+   *          description: Username to use for login.
+   *          in: formData
+   *          required: true
+   *          type: string
+   *        - name: password
+   *          description: User's password.
+   *          in: formData
+   *          required: true
+   *          type: string
+   *      responses:
+   *        200:
+   *          description: login
+   */
+  app.post('/login', loginHandler);
+};
