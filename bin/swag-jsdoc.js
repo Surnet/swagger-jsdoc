3317ac241825b3946029fd6e97242045cec906e4
Kalin Chernev <kalin.chernev@gmail.com>
2016-06-30 18:22:31 +0000

    More flexible input

--- bin/swag-jsdoc.js
+++ bin/swag-jsdoc.js
@@ -6,15 +6,33 @@
  * Module dependencies.
  */
 var program = require('commander');
+var fs = require('fs');
+var path = require('path');
+var input = process.argv.slice(2);
 
 program
   .version('1.3.0')
-  .option('-d, --definition', 'Swagger definition')
-  .option('-a, --apis', 'Input files or folders with API documentation')
-  .option('-o, --output', 'Output file with Swagger specification')
+  .usage('[options] <path ...>')
+  .option('-d, --definition <swaggerDef.js>', 'Input swagger definition.')
+  .option('-o, --output [swaggerSpec.json]', 'Output swagger specification.')
   .parse(process.argv);
 
 // If no arguments provided, display help menu.
-if (!process.argv.slice(2).length) {
+if (!input.length) {
   program.help();
+}
+
+// No swagger definition file provided by input.
+if (program.args.length) {
+  console.log(program.args);
+  console.log(program.apis);
+//   fs.readdir(folder, function(err, files) {
+// if (err) throw Error(err);
+// files.forEach(function(file){
+//   var fileExtension = path.extname(file);
+//   if ((fileExtension.substring(1, fileExtension.length)) == extension) {
+//     console.log(file);
+//   }
+// });
+// });
 }
