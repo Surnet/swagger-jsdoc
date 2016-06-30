cb344e04b055159b29a26d046e9edfdab35d90b1
Kalin Chernev <kalin.chernev@gmail.com>
2016-06-30 20:52:06 +0000

    Time to parse the definition file

--- bin/swag-jsdoc.js
+++ bin/swag-jsdoc.js
@@ -29,17 +29,19 @@ if (!program.definition) {
 }
 
 // Definition file is specified:
-fs.readFile(program.definition, 'utf-8', function(err, data) {
+fs.readFile(program.definition, 'utf-8', function (err, data) {
   if (err) console.error(err);
   // Check if the definition provided can be used:
   if (data == undefined) {
     console.error('Definition file provided is not good.');
     return;
-  } 
+  }
   // Check whether the definition file is actually a usable .js file
   if (path.extname(program.definition) != '.js') {
     console.error('Definition file should be a .js file.')
     return;
   }
-  
+  // Read the definition file:
+  // Example:
+  console.log(data);
 });
