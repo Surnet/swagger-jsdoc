068e2b48d40483f19d9d182c788900ba2f5fcc4e
Kalin Chernev <kalin.chernev@gmail.com>
2016-08-08 11:03:27 +0000

    Help menu tests.

--- test/cli-test.js
+++ test/cli-test.js
@@ -2,16 +2,25 @@
 
 // Dependencies.
 var exec = require('child_process').exec;
+var chai = require('chai');
+var expect = chai.expect;
 
 describe('command line interface', function () {
 
   it('help menu works', function (done) {
     var helpCommand = process.env.PWD + '/bin/swagger-jsdoc.js -h';
-    console.log(helpCommand);
     exec(helpCommand, function (error, stdout, stderr) {
-      console.log(stdout);
+      expect(stdout).to.contain('Usage:');
+      done();
     });
-    done();
+  });
+
+  it('help menu is default fallback when no arguments', function (done) {
+    var helpCommand = process.env.PWD + '/bin/swagger-jsdoc.js';
+    exec(helpCommand, function (error, stdout, stderr) {
+      expect(stdout).to.contain('Usage:');
+      done();
+    });
   });
 
 
