0a4ddc5041b5af21a4fba591cc00886da227f455
Kalin Chernev <kalin.chernev@gmail.com>
2016-08-31 20:50:26 +0000

    Testing the output of the main module.

--- test/parser.js
+++ test/parser.js
@@ -2,15 +2,20 @@
 
 // Dependencies
 var request = require('supertest');
+var chai = require('chai').should();
+
 var app = require('../example/app');
+var swaggerJSDoc = require('../');
+var swaggerDefinition = require('../example/swaggerDef');
+
 
 describe('swagger parsing features', function () {
 
-  it('example should output a specification with tags property', function (done) {
+  it('example app should output a specification with tags property via en endpoint', function (done) {
     request(app)
       .get('/api-docs.json')
       .on('response', function (response) {
-        response.body.hasOwnProperty('tags');
+        response.body.should.contain.tags;
       })
       .end(function (err) {
         if (err) {
@@ -18,6 +23,20 @@ describe('swagger parsing features', function () {
         }
         done();
       });
+  });
+
+  it('main module parser should ouput a specification with tags property in a direct call', function (done) {
+    var exampleApis = ['../example.routes.js', '../example.route2.js'];
+    // Options for the swagger docs
+    var options = {
+      // Import swaggerDefinitions
+      swaggerDefinition: swaggerDefinition,
+      // Path to the API docs
+      apis: exampleApis,
+    };
+    var swaggerSpecification = swaggerJSDoc(options);
+    swaggerSpecification.should.contain.tags;
+    done();
   });
 
 });
