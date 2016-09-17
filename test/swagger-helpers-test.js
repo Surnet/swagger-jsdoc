ff58ba059b5c6f0342bbc9e7e5a4997b472137db
Kalin Chernev <kalin.chernev@gmail.com>
2016-09-17 18:45:06 +0000

    addDataToSwaggerObject requires parameters.

---
+++ test/swagger-helpers-test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// The hinter will deny a lot of the chai syntax (W030).
+/* jshint ignore:start */
+
+// Dependencies.
+var swaggerHelpers = require('../lib/swagger-helpers');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('swagger-helpers submodule', function () {
+
+  it('should have a method addDataToSwaggerObject()', function (done) {
+    expect(swaggerHelpers).to.include.keys('addDataToSwaggerObject');
+    expect(swaggerHelpers.addDataToSwaggerObject).to.be.function;
+    done();
+  });
+
+  it('addDataToSwaggerObject() should require correct input', function (done) {
+    expect(swaggerHelpers.addDataToSwaggerObject).to.throw(Error);
+    done();
+  });
+
+  it('should have a method swaggerizeObj()', function (done) {
+    expect(swaggerHelpers).to.include.keys('swaggerizeObj');
+    expect(swaggerHelpers.swaggerizeObj).to.be.function;
+    done();
+  });
+
+});
+/* jshint ignore:end */
