4522746949a2f43e5d9fb1c25bb5e8ae7ed956bf
Will Vincent <will@willvincent.com>
2018-06-27 18:15:18 -0500

    Fix Issue #78 - Prevent paths overriding each other

--- test/swagger-helpers-test.js
+++ test/swagger-helpers-test.js
@@ -78,5 +78,23 @@ describe('swagger-helpers submodule', function () {
       done();
   });
 
+  it('paths should not override each other', function (done) {
+    var swagger = require('../lib');
+
+    var testObject = {
+      swaggerDefinition: {
+        info: { // API informations (required)
+          title: 'Hello World', // Title (required)
+          version: '1.0.0', // Version (required)
+          description: 'A sample API', // Description (optional)
+        }
+      },
+      apis: ['./**/*/external/*.yml']
+    }
+    testObject = swagger(testObject);
+    expect(testObject.responses.api).to.include.keys(['foo', 'bar'])
+    done()
+  })
+
 });
 /* jshint ignore:end */
