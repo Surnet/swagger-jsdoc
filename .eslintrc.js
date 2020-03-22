598b7e7e405ff224e1b38be1cdacac2f7918989d
Kalin Chernev <kalin.chernev@gmail.com>
2020-03-22 13:32:22 +0100

    Upgrade deps + bump to 3.6.0

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  rules: {
+    'func-names': 'off',
+    'no-console': 'off',
+  },
+};
--- bin/swagger-jsdoc.js
+++ bin/swagger-jsdoc.js
@@ -7,7 +7,7 @@ const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 const jsYaml = require('js-yaml');
-const swaggerJSDoc = require('../');
+const swaggerJSDoc = require('..');
 const pkg = require('../package.json');
 
 // Useful input.
@@ -44,7 +44,7 @@ function createSpecification(swaggerDefinition, apis, fileName) {
     swaggerSpec = JSON.stringify(swaggerJSDoc(options), null, 2);
   }
 
-  fs.writeFile(fileName, swaggerSpec, err => {
+  fs.writeFile(fileName, swaggerSpec, (err) => {
     if (err) {
       throw err;
     }
