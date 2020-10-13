41c410fa8daeaed1d5a5cb45dda7aaee43b51021
Kevin Martin <kvn.p.mrtn@gmail.com>
2020-10-13 06:47:55 -0700

    feat: add coffeescript support (#52)
    It's now possible to include your `.coffee` files into the result swagger specification.
    
    Example:
    
    ```sh
    swagger-jsdoc.js -d example/v2/swaggerDef.js example/v2/route.coffee
    ```

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -1,6 +1,7 @@
 /* eslint no-unused-expressions: 0 */
 const specHelper = require('../lib/helpers/specification');
 const hasEmptyProperty = require('../lib/helpers/hasEmptyProperty');
+const parseApiFileContent = require('../lib/helpers/parseApiFileContent');
 
 const swaggerObject = require('./files/v2/swaggerObject.json');
 const testData = require('./files/v2/testData');
@@ -91,17 +92,126 @@ describe('Helpers', () => {
     });
   });
 
-  it('hasEmptyProperty() identifies object with an empty object or array as property', () => {
-    const invalidA = { foo: {} };
-    const invalidB = { foo: [] };
-    const validA = { foo: { bar: 'baz' } };
-    const validB = { foo: ['¯_(ツ)_/¯'] };
-    const validC = { foo: '¯_(ツ)_/¯' };
+  describe('hasEmptyProperty', () => {
+    it('identifies object with an empty object or array as property', () => {
+      const invalidA = { foo: {} };
+      const invalidB = { foo: [] };
+      const validA = { foo: { bar: 'baz' } };
+      const validB = { foo: ['¯_(ツ)_/¯'] };
+      const validC = { foo: '¯_(ツ)_/¯' };
 
-    expect(hasEmptyProperty(invalidA)).toBe(true);
-    expect(hasEmptyProperty(invalidB)).toBe(true);
-    expect(hasEmptyProperty(validA)).toBe(false);
-    expect(hasEmptyProperty(validB)).toBe(false);
-    expect(hasEmptyProperty(validC)).toBe(false);
+      expect(hasEmptyProperty(invalidA)).toBe(true);
+      expect(hasEmptyProperty(invalidB)).toBe(true);
+      expect(hasEmptyProperty(validA)).toBe(false);
+      expect(hasEmptyProperty(validB)).toBe(false);
+      expect(hasEmptyProperty(validC)).toBe(false);
+    });
+  });
+
+  describe('parseApiFileContent', () => {
+    it('should extract jsdoc comments inside .js files', () => {
+      const fileContent = `
+        // Sets up the routes.
+        module.exports.setup = function (app) {
+          /**
+           * @swagger
+           * tags:
+           *   name: Users
+           *   description: User management and login
+           */
+
+          /**
+           * @swagger
+           * /users:
+           *   post:
+           *     description: Returns users
+           *     tags: [Users]
+           *     produces:
+           *       - application/json
+           *     parameters:
+           *       - $ref: '#/parameters/username'
+           *     responses:
+           *       200:
+           *         description: users
+           */
+          app.post('/users', (req, res) => {
+            res.json(req.body);
+          });
+        };
+      `;
+
+      expect(parseApiFileContent(fileContent, '.js')).toEqual({
+        yaml: [],
+        jsdoc: [
+          {
+            description: '',
+            tags: [
+              {
+                title: 'swagger',
+                description:
+                  'tags:\n  name: Users\n  description: User management and login',
+              },
+            ],
+          },
+          {
+            description: '',
+            tags: [
+              {
+                title: 'swagger',
+                description:
+                  "/users:\n  post:\n    description: Returns users\n    tags: [Users]\n    produces:\n      - application/json\n    parameters:\n      - $ref: '#/parameters/username'\n    responses:\n      200:\n        description: users",
+              },
+            ],
+          },
+        ],
+      });
+    });
+
+    it('should extract coffeescript comments inside .coffee files', () => {
+      const fileContent = `
+      # Coffeescript Example
+
+      ###
+      * @swagger
+      * /login:
+      *   post:
+      *     description: Login to the application
+      *     produces:
+      *       - application/json
+      *     parameters:
+      *       - name: username
+      *         description: Username to use for login.
+      *         in: formData
+      *         required: true
+      *         type: string
+      *       - name: password
+      *         description: User's password.
+      *         in: formData
+      *         required: true
+      *         type: string
+      *     responses:
+      *       200:
+      *         description: login
+      ###
+      app.post '/login', (req, res) ->
+        res.json req.body
+    `;
+
+      expect(parseApiFileContent(fileContent, '.coffee')).toEqual({
+        yaml: [],
+        jsdoc: [
+          {
+            description: '/',
+            tags: [
+              {
+                title: 'swagger',
+                description:
+                  "/login:\n  post:\n    description: Login to the application\n    produces:\n      - application/json\n    parameters:\n      - name: username\n        description: Username to use for login.\n        in: formData\n        required: true\n        type: string\n      - name: password\n        description: User's password.\n        in: formData\n        required: true\n        type: string\n    responses:\n      200:\n        description: login",
+              },
+            ],
+          },
+        ],
+      });
+    });
   });
 });
