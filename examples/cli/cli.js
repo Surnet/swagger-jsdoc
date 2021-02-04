c089c3eca73c0705b489f3081f7fcdcedbe2b2c5
Kalin Chernev <kalin.chernev@gmail.com>
2021-02-04 08:48:47 +0100

    run without caching

--- examples/cli/cli.js
+++ examples/cli/cli.js
@@ -5,18 +5,23 @@ import { pathToFileURL } from 'url';
 import { loadDefinition } from '../../src/utils.js';
 import swaggerJsdoc from '../../src/lib.js';
 
-// Handle CLI arguments in your preferred way.
-// @see https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/
+/**
+ * Handle CLI arguments in your preferred way.
+ * @see https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/
+ */
 const args = process.argv.slice(2);
 
-// Extract definition file.
-// It's always only 1.
-// The definition loader requires an absolute specifier with file:///
+/**
+ * Extract definition
+ * Pass an absolute specifier with file:/// to the loader.
+ * The relative and bare specifiers would be based on assumptions which are not stable.
+ * For example, if path from cli `examples/app/parameters.*` goes in, it will be assumed as bare, which is wrong.
+ */
 const definitionUrl = pathToFileURL(
   args.splice(
     args.findIndex((i) => i === '--definition'),
     2
-  )[1]
+  )[1] // Definition file is always only one.
 );
 
 // Because "Parsing error: Cannot use keyword 'await' outside an async function"
