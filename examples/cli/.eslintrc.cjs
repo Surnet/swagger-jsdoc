4c2178e915f8f55a303b7d44f7555eb6fb27f6a0
Kalin Chernev <kalin.chernev@gmail.com>
2021-02-28 12:26:17 +0100

    Remove rules inherited from airbnb which is not there any more

--- examples/cli/.eslintrc.cjs
+++ examples/cli/.eslintrc.cjs
@@ -13,12 +13,5 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:jest/recommended',
   ],
-  rules: {
-    'func-names': 'off',
-    'no-console': 'off',
-    'no-param-reassign': 'off',
-    'no-self-assign': 'off',
-    'no-restricted-syntax': 'off',
-    'guard-for-in': 'off',
-  },
+  rules: {},
 };
